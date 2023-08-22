f235ad38b65b949b06de22873f12ccf4a40898ea
harshitha-sn <harshithasn23@gmail.com>
2023-08-22 20:03:33 +0530

    multiplication function added

--- python progs/func.py
+++ python progs/func.py
@@ -1,8 +1,14 @@
 def add(a,b):
     return(a+b)
+
 def sub(a,b):
     return(a-b)
+
+def mul(a,b):
+    return(a*b)
+
 a=int(input("enter num1:"))
 b=int(input("enter num2:"))
 print("sum=",(a+b))
 print("difference=",(a-b))
+print("product=",(a*b))
